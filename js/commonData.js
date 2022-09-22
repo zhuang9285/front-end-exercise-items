9b321bf4b5d2657ee19897ca1a4abc9612ce77ca
zhuang <coderzhuang@163.com>
2022-09-23 04:01:40 +0800

    add data.js tag

--- js/commonData.js
+++ js/commonData.js
@@ -1,1 +1,82 @@
 // 网站全局数据文件
+// 网站更新信息
+let web_info = [
+  {
+    version:'0.3',
+  },
+];
+
+// 分类标签tag，显示在
+let tag = [
+  {
+    id:'01',
+    name:"web",
+    zh_name:"前端"
+  },{
+    id:'02',
+    name:"Linux",
+    zh_name:''
+  },{
+    id:'03',
+    name:'respeberry PI',
+    zh_name:'树莓派'
+  },{
+    id:'04',
+    name:'Project',
+    zh_name:'项目'
+  },{
+    id:'05',
+    name:'Life',
+    zh_name:'生活'
+  },
+  // 二级标签
+  {
+    id:'0101',
+    name:"HTML",
+    zh_name:""
+  },{
+    id:'0102',
+    name:"Css",
+    zh_name:""
+  },{
+    id:'0103',
+    name:'JavaScript',
+    zh_name:''
+  },{
+    id:'0104',
+    name:'Vue',
+    zh_name:''
+  },{
+    id:'0105',
+    name:'React',
+    zh_name:''
+  },{
+    id:'0106',
+    name:'Node',
+    zh_name:''
+  },{
+    id:'0107',
+    name:'Npm',
+    zh_name:''
+  },{
+    id:'0108',
+    name:'Canvas',
+    zh_name:''
+  },{
+    id:'0109',
+    name:'Svg',
+    zh_name:''
+  },{
+    id:'0110',
+    name:'Pug',
+    zh_name:''
+  },{
+    id:'0111',
+    name:'Algorithm',
+    zh_name:'算法'
+  },{
+    id:'0199',
+    name:'others',
+    zh_name:'其他'
+  },  
+];
